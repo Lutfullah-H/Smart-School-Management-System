67287605c1a8b2ae04e79bded5d552daf9628574
Lutfullah <lutfullah.ha@gmail.com>
2026-07-27 15:14:58 +0430

    Create public website routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', function () 
+{
+    return "This is the home page";
+}) ->name('home');
+
+
+Route::get('/about/{id}', function ($id) 
+{
+    return "This is about page and your id is ".$id;
+})->name('about');
+
+Route::get('/features/{name?}', function ($name = "user") 
+{
+    return "this is features page and you are ".$name;
+})->name('features');
+
+Route::get('/pricing', function () 
+{
+    return "this is pricing page";
+})->name('pricing');
+
+Route::get('/contact', function () 
+{
+    return "this is contact page" ;
+})->name('contact');
